8a8269d0d1f3659dd35b044cc9ee138be8f4620e
EugeneLesnov <zheka.lesnov1996@gmail.com>
2021-01-28 22:25:55 +0300

    feat: added basic project structure

---
+++ src/main/java/com/github/eugenelesnov/processor/AnnotationProcessor.java
@@ -0,0 +1,39 @@
+package com.github.eugenelesnov.processor;
+
+import com.github.eugenelesnov.annotation.OptionalGetter;
+
+import javax.annotation.processing.AbstractProcessor;
+import javax.annotation.processing.RoundEnvironment;
+import javax.annotation.processing.SupportedAnnotationTypes;
+import javax.annotation.processing.SupportedSourceVersion;
+import javax.lang.model.SourceVersion;
+import javax.lang.model.element.Element;
+import javax.lang.model.element.TypeElement;
+import java.util.Set;
+
+/**
+ * @author Eugene Lesnov
+ */
+@SupportedAnnotationTypes({"com.github.com.github.eugenelesnov.annotation.OptionalGetter"})
+@SupportedSourceVersion(SourceVersion.RELEASE_11)
+public class AnnotationProcessor extends AbstractProcessor {
+
+    @Override
+    public boolean process(Set<? extends TypeElement> annotations, RoundEnvironment roundEnvironment) {
+        if (annotations.size() == 0) {
+            return false;
+        }
+
+        Set<? extends Element> elements = roundEnvironment.getElementsAnnotatedWith(OptionalGetter.class);
+        elements.forEach(element -> {
+
+            OptionalGetter optionalGetter = element.getAnnotation(OptionalGetter.class);
+
+            if (element.getKind().isField()) {
+
+            }
+        });
+
+        return false;
+    }
+}
